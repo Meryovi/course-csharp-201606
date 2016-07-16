7a319713b6ae1c0450a097343e74dd6f1dbdb4f6
Meryovi De Dios <Meryovi De Dios>
2016-07-16 11:52:00 -0400

    Contenido de la clase del sabado 16 de julio. Inicio con desarrollo Web en ASP.NET MVC.

---
+++ src/clase7/webApp/webApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webApp.Models;
+
+namespace webApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var persona = new Persona()
+            {
+                Nombre = "Juan",
+                Apellido = "Perez"
+            };
+
+
+            ViewBag.Persona = persona;
+
+            return View(persona);
+        }
+
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            var persona = new Persona();
+
+            return View(persona);
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Mensaje = "La información fue digitada correctamente.";
+            }
+
+            return View(persona);
+        }
+    }
+}
---
+++ src/clase7/webApp/webApp/Models/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webApp.Models
+{
+    public class Persona
+    {
+        [Required]
+        [StringLength(30)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Apellido { get; set; }
+    }
+}
