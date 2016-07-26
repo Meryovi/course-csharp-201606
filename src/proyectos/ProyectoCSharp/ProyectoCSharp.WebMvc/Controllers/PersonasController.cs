dfde8d1d2357e27f3d067d914855c2b084882879
Meryovi De Dios <Meryovi De Dios>
2016-07-25 21:56:47 -0400

    Comentarios clase sabado 23.

--- src/proyectos/ProyectoCSharp/ProyectoCSharp.WebMvc/Controllers/PersonasController.cs
+++ src/proyectos/ProyectoCSharp/ProyectoCSharp.WebMvc/Controllers/PersonasController.cs
@@ -7,28 +7,42 @@ using ProyectoCSharp.Libreria.Repositorios;
 
 namespace ProyectoCSharp.WebMvc.Controllers
 {
+    /// <summary>
+    /// Controlador de personas.
+    /// En este colocaremos nuestra lógica para gestionar la información
+    /// de las personas registradas en nuestro sistema.
+    /// </summary>
     public class PersonasController : Controller
     {
+        /// <summary>
+        /// Instancia del repositorio de personas que creamos antes.
+        /// Podemos reutilizar las clases creadas en el proyecto WinForms.
+        /// </summary>
         private RepositorioPersonasEF repositorioPersonas;
 
         public PersonasController()
         {
+            // Inicializamos el objeto.
             repositorioPersonas = new RepositorioPersonasEF();
         }
 
         // GET: Personas
         public ActionResult Index()
         {
+            // Buscamos las personas registradas.
             var personas = repositorioPersonas.BuscarPersonas();
 
+            // Y las cargamos en la vista.
             return View(personas);
         }
 
         // GET: Personas/Details/5
         public ActionResult Details(string id)
         {
+            //  Buscamos una persona en especifico.
             var persona = repositorioPersonas.BuscarPersona(id);
 
+            // Y la cargamos en la vista.
             return View(persona);
         }
 
